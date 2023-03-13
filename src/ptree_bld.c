f90cfe4418110749c9f001336f628ca4e1fbd3ca
digital-nitrate <ea@luehrmann.com>
2023-03-12 23:17:50 -0600

    mem safe (mostly) ptree

--- src/ptree_bld.c
+++ src/ptree_bld.c
@@ -4,13 +4,13 @@
 #include "cfg.h"
 #include "ptree.h"
 
-static unsigned int const* ptree_bld_inner(ptree* restrict tree, cfg const* restrict grammar, unsigned int const* restrict str, unsigned int const* restrict end) {
+static unsigned int const* ptree_bld_inner(ptree* restrict tree, cfg_sid own, cfg const* restrict grammar, unsigned int const* restrict str, unsigned int const* restrict end) {
 	if (str == end) return NULL;
-	if (tree->own.term) {
-		if (*str != tree->own.id) return NULL;
+	if (own.term) {
+		if (*str != own.id) return NULL;
 		++str;
 	} else {
-		struct cfg_nterm const* const sym = &(grammar->nterms.data[tree->own.id]);
+		struct cfg_nterm const* const sym = &(grammar->nterms.data[own.id]);
 		unsigned int rule = sym->ll1[*str];
 		if (rule == UINT_MAX || rule == UINT_MAX - 1) return NULL;
 		struct cfg_rule const* const r = &(sym->rules.data[rule]);
@@ -18,18 +18,21 @@ static unsigned int const* ptree_bld_inner(ptree* restrict tree, cfg const* rest
 		ptree* inner = malloc((sizeof *inner) * cnt);
 		if (inner == NULL) return NULL;
 		for (size_t i = 0; i < cnt; ++i) {
-			inner[i].own = r->syms.data[i];
-			str = ptree_bld_inner(inner + i, grammar, str, end);
-			if (str == NULL) return NULL;
+			str = ptree_bld_inner(inner + i, r->syms.data[i], grammar, str, end);
+			if (str == NULL) {
+				for (size_t j = 0; j < i; ++j) ptree_free(inner + j, grammar);
+				free(inner);
+				return NULL;
+			};
 		}
 		tree->con = inner;
 		tree->rule = rule;
 	}
+	tree->own = own;
 	return str;
 }
 
 int ptree_bld(ptree* restrict tree, cfg const* restrict grammar, unsigned int const* restrict str, size_t len) {
-	tree->own = grammar->start;
-	if (ptree_bld_inner(tree, grammar, str, str + len) != str + len) return 1;
+	if (ptree_bld_inner(tree, grammar->start, grammar, str, str + len) != str + len) return 1;
 	return 0;
 }
